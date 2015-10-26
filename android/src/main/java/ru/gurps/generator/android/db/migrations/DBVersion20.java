ca9b5abbc8d1424a8e1c198c88aba7ffef77ad33
Игорь Кутявин <igor.kutavin@saltpepper.ru>
2015-10-26 14:42:44 +0300

    Add CharactersSpell model

---
+++ android/src/main/java/ru/gurps/generator/android/db/migrations/DBVersion20.java
@@ -0,0 +1,30 @@
+package ru.gurps.generator.android.db.migrations;
+
+import com.b50.migrations.AbstractMigration;
+
+public class DBVersion20 extends AbstractMigration {
+
+    public void up() {
+        execSQL("create table CharactersSpells" +
+                "(" +
+                "_id integer auto_increment primary key not null," +
+                "characterId integer not null," +
+                "spellId integer not null," +
+                "cost integer not null," +
+                "level integer not null," +
+                "foreign key (characterId) references Characters (id)," +
+                "foreign key (spellId) references Spells (id)" +
+                ");");
+
+        execSQL("create index character_id_characters_spells_index on CharactersSpells" +
+                "(characterId);");
+        execSQL("create index spell_id_characters_spells_index on CharactersSpells" +
+                "(spellId);");
+    }
+
+    public void down() {
+        execSQL("DROP TABLE CharactersSpells");
+    }
+
+}
+
---
+++ android/src/main/java/ru/gurps/generator/android/models/characters/CharactersSpell.java
@@ -0,0 +1,26 @@
+package ru.gurps.generator.android.models.characters;
+
+import android.content.Context;
+
+import ru.gurps.generator.android.db.Model;
+
+public class CharactersSpell extends Model {
+    public long _id;
+    public Integer characterId;
+    public Integer spellId;
+    public Integer level;
+    public Integer cost;
+
+    public CharactersSpell(Context context) {
+        super(context);
+    }
+
+    public CharactersSpell(Context context, Integer characterId, Integer spellId, Integer level,
+                           Integer cost) {
+        super(context);
+        this.characterId = characterId;
+        this.spellId = spellId;
+        this.level = level;
+        this.cost = cost;
+    }
+}
