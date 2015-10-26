c332a99a6e3e14835ee0a9065228d227d26a9384
Игорь Кутявин <igor.kutavin@saltpepper.ru>
2015-10-26 14:04:28 +0300

    Add CharactersCultura model

---
+++ android/src/main/java/ru/gurps/generator/android/db/migrations/DBVersion14.java
@@ -0,0 +1,29 @@
+package ru.gurps.generator.android.db.migrations;
+
+import com.b50.migrations.AbstractMigration;
+
+public class DBVersion14 extends AbstractMigration {
+
+    public void up() {
+        execSQL("create table CharactersCulturas" +
+                "(" +
+                "_id integer auto_increment primary key not null," +
+                "characterId integer not null," +
+                "culturaId integer not null," +
+                "cost integer default 0 not null," +
+                "foreign key (characterId) references Characters (id)," +
+                "foreign key (culturaId) references Culturas (id)" +
+                ");");
+
+        execSQL("create index character_id_characters_culturas_index on CharactersCulturas" +
+                "(characterId);");
+        execSQL("create index cultura_id_characters_culturas_index on CharactersCulturas" +
+                "(culturaId);");
+    }
+
+    public void down() {
+        execSQL("DROP TABLE CharactersCulturas");
+    }
+
+}
+
