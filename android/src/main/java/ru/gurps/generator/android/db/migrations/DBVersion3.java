f43b54f5f3865961720d0a1fa3b2144e543d862d
Игорь Кутявин <igor.kutavin@saltpepper.ru>
2015-10-26 11:39:42 +0300

    Add Modifier model and data

---
+++ android/src/main/java/ru/gurps/generator/android/db/migrations/DBVersion3.java
@@ -0,0 +1,53 @@
+package ru.gurps.generator.android.db.migrations;
+
+import com.b50.migrations.AbstractMigration;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+
+public class DBVersion3 extends AbstractMigration {
+
+    public void up() {
+        execSQL("CREATE TABLE Modifiers" +
+                "(" +
+                "_id integer auto_increment primary key not null," +
+                "name varchar(255) not null," +
+                "nameEn varchar(255)," +
+                "cost integer not null," +
+                "description clob," +
+                "maxLevel integer not null," +
+                "combat boolean not null," +
+                "improving boolean not null" +
+                ");");
+
+        try {
+            String tableName = "Modifiers";
+            String columns = "_id, name, nameEn, cost, description, maxLevel, combat, improving";
+            String str1 = "INSERT INTO " + tableName + " (" + columns + ") values(";
+            String str2 = ");";
+
+            InputStream in = this.getClass().getClassLoader()
+                    .getResourceAsStream("assets/csv/modifiers.csv");
+            CSVReader reader = new CSVReader(new InputStreamReader(in));
+            String[] str;
+
+            while ((str = reader.readNext()) != null) {
+                execSQL(str1 + "\"" + str[0] + "\"," + "\"" + str[1] + "\"," + "\"" + str[2]
+                        + "\"," + "\"" + str[3] + "\"," + "\"" + str[4] + "\"," + "\"" + str[5]
+                        + "\"," + "\"" + str[6] + "\"," + "\"" + str[7] + "\"" + str2);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void down() {
+        execSQL("DROP TABLE Modifiers");
+    }
+
+}
+
---
+++ android/src/main/java/ru/gurps/generator/android/models/rules/Modifier.java
@@ -0,0 +1,36 @@
+package ru.gurps.generator.android.models.rules;
+
+import android.content.Context;
+
+import ru.gurps.generator.android.db.Model;
+
+public class Modifier extends Model {
+    public long _id;
+    public String name;
+    public String nameEn;
+    public Integer cost;
+    public String description;
+    public Integer maxLevel;
+    public Boolean combat;
+    public Boolean improving;
+    @Ignore public Integer level;
+
+    public Modifier(Context context) {
+        super(context);
+    }
+
+    public Modifier(Context context, long _id, String name, String nameEn, Integer cost,
+                    String description, Integer maxLevel, Boolean combat,
+                    Boolean improving, Integer level) {
+        super(context);
+        this._id = _id;
+        this.name = name;
+        this.nameEn = nameEn;
+        this.cost = cost;
+        this.description = description;
+        this.maxLevel = maxLevel;
+        this.combat = combat;
+        this.improving = improving;
+        this.level = level;
+    }
+}
