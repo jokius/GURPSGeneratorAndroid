8bef6f4b5c4d64ba40a04f1169a9879587c89e29
Игорь Кутявин <igor.kutavin@saltpepper.ru>
2015-10-29 13:51:51 +0300

    Change all functions; add cv(), save()

--- android/src/main/java/ru/gurps/generator/android/db/Model.java
+++ android/src/main/java/ru/gurps/generator/android/db/Model.java
@@ -3,7 +3,6 @@ package ru.gurps.generator.android.db;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -19,7 +18,8 @@ public class Model extends DatabaseHelper {
 
     @Target(FIELD)
     @Retention(RetentionPolicy.RUNTIME)
-    public @interface Ignore {}
+    public @interface Ignore {
+    }
 
     protected String table = this.getClass().getSimpleName() + "s";
 
@@ -29,48 +29,52 @@ public class Model extends DatabaseHelper {
     }
 
     public Model create() {
-        ContentValues cv = new ContentValues();
-        for (Field field : this.getClass().getDeclaredFields())
-            try {
-                if (!field.isAnnotationPresent(Ignore.class) && field.get(this) != null && !field.getName().equals("_id")) {
-                    if (String.class.isAssignableFrom(field.getType()))
-                        cv.put(field.getName(), (String) field.get(this));
-                    else if (Integer.class.isAssignableFrom(field.getType()))
-                        cv.put(field.getName(), (Integer) field.get(this));
-                    else if (Double.class.isAssignableFrom(field.getType()))
-                        cv.put(field.getName(), (Double) field.get(this));
-                    else if (Boolean.class.isAssignableFrom(field.getType()))
-                        cv.put(field.getName(), (Boolean) field.get(this));
-                }
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-
-        SQLiteDatabase db = getWritableDatabase();
-        long id = db.insert(table, null, cv);
-        Cursor cursor = db.query(table, null, "_id = " + id, null, null, null, null);
+        long id = db().insert(table, null, cv());
+        Cursor cursor = db().query(table, null, "_id = " + id, null, null, null, null);
         cursor.moveToFirst();
-        Model model = setModel(cursor);
-        db.close();
-        return model;
+        return setModel(cursor);
     }
 
     public ArrayList all() {
         ArrayList list = new ArrayList();
-        SQLiteDatabase db = getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + table, null);
+        Cursor cursor = db().rawQuery("SELECT * FROM " + table, null);
         while (cursor.moveToNext()) {
             list.add(setModel(cursor));
         }
-        db.close();
         return list;
     }
 
     public boolean delete() {
-        SQLiteDatabase db = getWritableDatabase();
-        db.delete(table, "_id=" + id(), null);
-        db.close();
+        db().delete(table, "_id=" + id(), null);
         return true;
+    }
+
+    public boolean save() {
+        db().update(table, cv(), "_id = ?", new String[]{Long.toString(id())});
+        return true;
+    }
+
+    private ContentValues cv() {
+        ContentValues cv = new ContentValues();
+        for (Field field : this.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(Ignore.class)) continue;
+            try {
+                if (field.get(this) == null || field.getName().equals("_id")) continue;
+                if (String.class.isAssignableFrom(field.getType()))
+                    cv.put(field.getName(), (String) field.get(this));
+                else if (Integer.class.isAssignableFrom(field.getType())) {
+                    cv.put(field.getName(), (Integer) field.get(this));
+                }
+                else if (Double.class.isAssignableFrom(field.getType()))
+                    cv.put(field.getName(), (Double) field.get(this));
+                else if (Boolean.class.isAssignableFrom(field.getType()))
+                    cv.put(field.getName(), (Boolean) field.get(this));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return cv;
     }
 
     private Model setModel(Cursor cursor) {
@@ -78,7 +82,7 @@ public class Model extends DatabaseHelper {
         try {
             model = this.getClass().getDeclaredConstructor(Context.class).newInstance(context);
             for (Field field : model.getClass().getDeclaredFields()) {
-                if(!field.isAnnotationPresent(Ignore.class)) {
+                if (!field.isAnnotationPresent(Ignore.class)) {
                     if (String.class.isAssignableFrom(field.getType()))
                         field.set(model, cursor.getString(cursor.getColumnIndexOrThrow(field.getName())));
                     else if (Integer.class.isAssignableFrom(field.getType()))
@@ -87,7 +91,7 @@ public class Model extends DatabaseHelper {
                         field.set(model, cursor.getDouble(cursor.getColumnIndexOrThrow(field.getName())));
                     else if (Boolean.class.isAssignableFrom(field.getType()))
                         field.set(model, cursor.getInt(cursor.getColumnIndexOrThrow(field.getName())) > 0);
-                    else if (Long.class.isAssignableFrom(field.getType())){
+                    else if (Long.class.isAssignableFrom(field.getType())) {
                         field.set(model, cursor.getLong(cursor.getColumnIndexOrThrow(field.getName())));
                     }
                 }
@@ -95,22 +99,22 @@ public class Model extends DatabaseHelper {
 
         } catch (IllegalAccessException e) {
             e.printStackTrace();
-        } catch(InstantiationException e) {
+        } catch (InstantiationException e) {
             e.printStackTrace();
-        } catch(InvocationTargetException e) {
+        } catch (InvocationTargetException e) {
             e.printStackTrace();
-        } catch(NoSuchMethodException e) {
+        } catch (NoSuchMethodException e) {
             e.printStackTrace();
         }
         return model;
     }
 
-    private long id(){
+    private long id() {
         try {
             return (Long) this.getClass().getDeclaredField("_id").get(this);
-        } catch ( NoSuchFieldException e) {
+        } catch (NoSuchFieldException e) {
             e.printStackTrace();
-        } catch(IllegalAccessException e) {
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
 
